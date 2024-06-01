67991fa9cce36c7a4708665bf7d1ab969d6c31a1
Paul Mac <plipp2804@gmail.com>
2024-05-31 18:14:48 -0700

    added work and school divs from resume

--- src/app/_components/typewriter.tsx
+++ src/app/_components/typewriter.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect, useRef } from "react";
 
 export default function Typewriter(props: { text: string }) {
   const [displayText, setDisplayText] = useState("");
+  const [showCursor, setShowCursor] = useState(true);
+  const doneTyping = useRef(false);
   const placeholder = useRef("");
   const text = props.text;
   const idx = useRef(0);
@@ -24,13 +26,22 @@ export default function Typewriter(props: { text: string }) {
       return () => clearInterval(nextChar);
     } else {
       interval.current = 550;
-      const nextChar = setInterval(removeDot, 550);
+      const nextChar = setTimeout(() => setShowCursor(false), 400);
+      doneTyping.current = true;
       return () => clearInterval(nextChar);
     }
   }, [displayText]);
+
+  useEffect(() => {
+    if (doneTyping.current) {
+      const blink = setInterval(() => setShowCursor(!showCursor), 550);
+      return () => clearInterval(blink);
+    }
+  }, [showCursor]);
   return (
     <span className="inline-block text-secondary-content text-9xl font-semibold w-[660px]">
       {displayText}
+      {showCursor && <>&#9646;</>}
     </span>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,44 @@ export default function HomePage() {
   return (
     <div className="flex flex-col mt-10 text-white">
       <div className="flex min-h-[80vh] w-full items-center justify-center">
-        <Typewriter text="Paul Lipp&#9646;" />
+        <Typewriter text="Paul Lipp" />
+      </div>
+      <div className="mx-10 font-semibold">
+        Education
+        <hr />
+      </div>
+      <div className="flex min-h-[40vh] w-full m-10">
+        <p className="text-lg font-semibold"> Oregon State University </p>
+      </div>
+      <div className="mx-10 font-semibold">
+        Work Experience
+        <hr />
+      </div>
+      <div className="flex flex-col gap-5 min-h-[40vh] w-full m-10">
+        <p className="text-lg font-semibold"> Kongregate - Junior Data Engineer </p>
+        <ul className="flex flex-col ml-5 break-normal whitespace-pre list-style-none gap-5">
+          <li>
+            Working with a team of data analysts and software engineers to resolve backend issues for stakeholders
+          </li>
+          <li>
+            Managing an ETL process to ensure quality data from various sources is available to be queried in our
+            <br />data warehouse
+          </li>
+          <li>
+            Maintain and improve internal tooling used by Analysts and Engineers
+          </li>
+        </ul>
+        <p className="text-lg font-semibold mt-20"> Kongregate - Junior Data Analyst </p>
+        <ul className="flex flex-col ml-5 break-normal whitespace-pre list-style-none gap-5">
+          <li>
+            Interpreting as well as visualizing large amounts of data by generating charts and tables that are used by
+            <br />Product Managers and Marketing teams to drive key product decisions
+          </li>
+          <li>
+            Helped migrate the companies entire BI stack onto a new, more modern platform, which helped improve user and 
+            <br />developer experience, while simultaneously cutting cost
+          </li>
+        </ul>
       </div>
     </div>
   );
